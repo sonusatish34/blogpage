58bb105965c01440677393120e7fd3bcbfcfdac1
sonusatish34 <sonusatish34@gmail.com>
2024-11-07 12:41:38 +0530

    swqsq

--- src/ckeditor5-custom-build/src/ckeditor.js
+++ src/ckeditor5-custom-build/src/ckeditor.js
@@ -2,7 +2,8 @@
  * @license Copyright (c) 2014-2022, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
-import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import ClassicEditor from '../../ckeditor5-custom-build/build/ckeditor';
+
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
 import AutoImage from '@ckeditor/ckeditor5-image/src/autoimage.js';
